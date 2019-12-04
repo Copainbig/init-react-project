a4dda1015538f92f5844721350b04878b1cf4042
Charles HUSQUIN <charles@mothership.local>
2019-12-03 22:59:33 -0800

    refactor code

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 #!/usr/bin/env node
-import commander from "commander";
+import program from "commander";
 import handler from "./lib/handler";
+import options from "./lib/options";
 
-commander.arguments('<project_name>')
-    .option('-r, --redux <use_redux>', 'If you need to use redux.')
-    .option('-b, --browser-cache <browser_cache>', 'Set the duration your HTML file will be cached by the browser (in seconds).')
-    .description('Simple and easily customizable react project creation CLI.')
-    .action((project_name) => { 
-        handler(project_name, commander["redux"], commander["browserCache"]) 
+
+// TODO: handle missing argument
+let cli = program
+    .arguments('<projectName>');
+
+options.forEach(elt => {
+    if (elt.default) {
+        cli = cli.option(elt.flag, elt.desc, elt.default);
+    } else {
+        cli = cli.option(elt.flag, elt.desc);
+    }
+});
+
+cli.description('Simple and easily customizable react project creation CLI.')
+    .action((projectName) => { 
+        const params = options.map(elt => ({...elt, value: program[elt.name]}) );
+        try{
+            handler(projectName, params) 
+        }catch (error) {
+            console.error(error);
+        }
     })
-    .parse(process.argv)
+    .parse(process.argv);
